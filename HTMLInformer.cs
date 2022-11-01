6db67661b3cd0c083f426ff9d9ba27ead834c9c5
vovavish <vovavishniakov856@gmail.com>
2022-11-01 21:59:06 +0300

    HTMLInformer in another file

---
+++ HTMLInformer.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+namespace HTMLInformer;
+
+class HTMLInformer
+{
+    public string HTML { get; set; }
+
+    public void DownloadHtml(string uri)
+    {
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                HTML = client.DownloadString(uri);
+            }
+            catch
+            {
+                Console.WriteLine("Something wrong. Check ref or your internet");
+                Environment.Exit(-1);
+            }
+        }
+    }
+
+    public List<string> GetAllAnchors()
+    {
+        return (from m in Regex.Matches(HTML, @"<a[^>]*>[^<]*</a>") select m.Value).ToList();
+    }
+
+    public List<string> GetAllReferences()
+    {
+        return (from m in Regex.Matches(HTML, @"<a[^>]*href=""(http.*)""[^>]*>[^<]*<\/a>")
+                select m.Groups[1].Value)
+                .ToList();
+    }
+
+    public List<string> GetAllEmails()
+    {
+        return (from m in Regex.Matches(HTML, @"[\w-\.\+]*@[\w-]*\.[\w]{2,4}") select m.Value).ToList();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace HTMLInformer;
 
@@ -70,43 +69,5 @@ internal class Program
         {
             Console.WriteLine("Unknown command. Try again.");
         }
-    }
-}
-
-class HTMLInformer
-{
-    public string HTML { get; set; }
-
-    public void DownloadHtml(string uri)
-    {
-        using (WebClient client = new WebClient())
-        {
-            try
-            {
-                HTML = client.DownloadString(uri);
-            }
-            catch
-            {
-                Console.WriteLine("Something wrong. Check ref or your internet");
-                Environment.Exit(-1);
-            }
-        }
-    }
-
-    public List<string> GetAllAnchors()
-    {
-        return (from m in Regex.Matches(HTML, @"<a[^>]*>[^<]*</a>") select m.Value).ToList();
-    }
-
-    public List<string> GetAllReferences()
-    {
-        return (from m in Regex.Matches(HTML, @"<a[^>]*href=""(http.*)""[^>]*>[^<]*<\/a>")
-                select m.Groups[1].Value)
-                .ToList();
-    }
-
-    public List<string> GetAllEmails()
-    {
-        return (from m in Regex.Matches(HTML, @"[\w-\.\+]*@[\w-]*\.[\w]{2,4}") select m.Value).ToList();
     }
 }
